60cbd5c625dee932b65ba4ffe9ef187371afd21f
Prachana <pralhadrana123@gmail.com>
2024-04-26 19:47:27 +0545

    implement the cart functionality

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -10,35 +10,114 @@ const addToCart = asyncHandler(async (req, res) => {
   const product = await Product.findById(productId);
   if (!product) throw new ApiError(404, "Product not found to add in the cart");
 
-  let cart = await Cart.findOne({ user: user._id });
-  if (!cart) {
-    // Create a new cart document for the user if it doesn't exist
-    cart = await Cart.create({
-      user: user._id,
-      products: [{ product: productId }],
-      totalAmount: product.price,
-    });
-  } else {
-    // Check if the product is already in the cart
-    const existingProductIndex = cart.products.findIndex(
-      (item) => item.product.toString() === productId
-    );
-    if (existingProductIndex !== -1) {
-      cart.products[existingProductIndex].quantity += 1;
-      cart.totalAmount += product.price;
-    } else {
-      cart.products.push({ product: productId });
-      cart.totalAmount += product.price;
+  let cart = await Cart.findOneAndUpdate(
+    { user: user._id },
+    {
+      $addToSet: {
+        // Add the product to the cart if it's not already present
+        products: { product: productId },
+      },
+    },
+    {
+      upsert: true, // Create a new cart if it doesn't exist
+      new: true, // Return the updated document
     }
-    await cart.save();
-  }
+  );
+
   return res
     .status(200)
     .json(new ApiResponse(200, cart, "Product added to cart"));
 });
 
 const getCartDetails = asyncHandler(async (req, res) => {
-  const cart = await Cart.find();
+  const user = req.user;
+  const cart = await Cart.findOne({ user: user._id });
+  if (!cart) throw new ApiError(404, "Cart not found for this user");
+
+  const pipeline = [
+    {
+      $match: { _id: cart._id },
+    },
+    {
+      $unwind: "$products",
+    },
+    {
+      $group: {
+        _id: {
+          cartId: "$_id",
+          productId: "$products.product",
+        },
+        quantity: { $sum: 1 },
+      },
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "_id.productId",
+        foreignField: "_id",
+        as: "productDetails",
+      },
+    },
+    {
+      $unwind: "$productDetails",
+    },
+    {
+      $addFields: {
+        cartProductQuantity: "$quantity",
+        totalAmount: {
+          $sum: {
+            $multiply: ["$productDetails.price", "$quantity"],
+          },
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$_id.cartId",
+        totalAmount: { $sum: "$totalAmount" }, // Sum the total amounts of all products
+        products: {
+          $push: {
+            _id: "$productDetails._id",
+            title: "$productDetails.title",
+            description: "$productDetails.description",
+            price: "$productDetails.price",
+            cartProductQuantity: "$cartProductQuantity",
+          },
+        },
+        cartQuantity: { $sum: "$quantity" }, // Calculate total quantity in the cart
+      },
+    },
+  ];
+
+  const result = await Cart.aggregate(pipeline);
+  if (!result || result.length === 0)
+    throw new ApiError(404, "Cart details not found");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result[0], "Cart details fetched successfully"));
 });
 
-export { addToCart, getCartDetails };
+const deleteCartItem = asyncHandler(async (req, res) => {
+  const { cartId, productId } = req.params;
+
+  // Find the cart by its ID
+  let cart = await Cart.findById(cartId);
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+  const existingItem = cart.products.find((item) => item.product == productId);
+  if (!existingItem) {
+    throw new ApiError(404, "Product not found in cart");
+  }
+
+  cart.products = cart.products.filter((item) => item.product != productId);
+
+  cart = await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Item removed from cart"));
+});
+
+export { addToCart, getCartDetails, deleteCartItem };
--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addToCart, getCartDetails } from "../controllers/cart.controller.js";
+import {
+  addToCart,
+  deleteCartItem,
+  getCartDetails,
+} from "../controllers/cart.controller.js";
 const cartRouter = Router();
 
 cartRouter.use(verifyJwt);
 
 cartRouter.route("/").get(getCartDetails);
 cartRouter.route("/add-to-cart/:productId").post(addToCart);
+cartRouter
+  .route("/delete-from-cart/:cartId/product/:productId")
+  .delete(deleteCartItem);
 export default cartRouter;

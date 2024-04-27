fb5bc305fd39bdcd81e4877f49b83f7c73c0b099
Prachana <pralhadrana123@gmail.com>
2024-04-27 11:28:27 +0545

    setup the backend

--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -6,6 +6,13 @@ import {
   getAllUsers,
   deleteUser,
 } from "../controllers/dashboard.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  productImageDelete,
+  updateProductDetails,
+  updateProductImage,
+} from "../controllers/product.controller.js";
 const dashboardRouter = Router();
 
 dashboardRouter.use(verifyJwt);
@@ -13,6 +20,28 @@ dashboardRouter.use(authorizeAdmin);
 
 dashboardRouter.route("/").get(getAllDetailsCount);
 dashboardRouter.route("/get-all-users").get(getAllUsers);
-dashboardRouter.route("/get-all-products").get(getAllProducts);
 dashboardRouter.route("/delete-user/:userId").delete(deleteUser);
+
+dashboardRouter.route("/get-all-products").get(getAllProducts);
+dashboardRouter.route("/product/create-new-product").post(
+  upload.fields([
+    {
+      name: "image",
+      maxCount: 3,
+    },
+  ]),
+  createProduct
+);
+dashboardRouter
+  .route("/product/update-product-details/:id")
+  .patch(updateProductDetails);
+
+dashboardRouter
+  .route("/product/update-product-image/:productId")
+  .patch(upload.array("image", 3), updateProductImage);
+dashboardRouter.route("/product/delete-product/:id").delete(deleteProduct);
+dashboardRouter
+  .route("/product/:id/product-image/:imageId")
+  .delete(productImageDelete);
+
 export default dashboardRouter;
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,34 +1,12 @@
 import { Router } from "express";
-import { authorizeAdmin, verifyJwt } from "../middlewares/auth.middleware.js";
 import {
-  createProduct,
-  deleteProduct,
   getAllProducts,
   getSingleProduct,
-  productImageDelete,
-  updateProductDetails,
-  updateProductImage,
 } from "../controllers/product.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
 const productRouter = Router();
-productRouter.use(verifyJwt);
-productRouter.use(authorizeAdmin);
-productRouter.route("/create-product").post(
-  upload.fields([
-    {
-      name: "image",
-      maxCount: 3,
-    },
-  ]),
-  createProduct
-);
+
 productRouter.route("/get-all-products").get(getAllProducts);
 productRouter.route("/get-single-product/:id").get(getSingleProduct);
-productRouter.route("/update-product-details/:id").patch(updateProductDetails);
-productRouter
-  .route("/update-product-image/:productId")
-  .patch(upload.array("image", 3), updateProductImage);
-productRouter.route("/delete-product/:id").delete(deleteProduct);
-productRouter.route("/:id/product-image/:imageId").delete(productImageDelete);
+
 export default productRouter;
